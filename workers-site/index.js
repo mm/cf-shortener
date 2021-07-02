eb014b810a88937504bf638b5c4c891552012cec
Matthew Mascioni <mascionim@gmail.com>
2021-07-02 16:29:59 -0400

    basic error handling on front-end

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,51 +1,53 @@
-import { Router } from 'itty-router';
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+import { Router } from 'itty-router'
+import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
 
-const router = Router();
+const router = Router()
 
-router.get('/:key', async ({ params }) => {
-  let link = await SHORTEN.get(params.key);
+router.get('/:key', async request => {
+  let link = await SHORTEN.get(request.params.key)
 
   if (link) {
     return new Response(null, {
-      headers: {'Location': link },
-      status: 301
-    });
+      headers: { Location: link },
+      status: 301,
+    })
   } else {
+    // If we couldn't find the link, request the 404 static page:
     return new Response('Key not found', {
-      status: 400
-    });
+      status: 400,
+    })
   }
-});
+})
 
-router.post('/links', async (request) => {
-  let array = new Uint32Array(1);
-  let randomValues = crypto.getRandomValues(array);
-  let unpadded = randomValues[0].toString(16);
-  let key = '00000000'.slice(unpadded.length) + unpadded;
+router.post('/links', async request => {
+  let array = new Uint32Array(1)
+  let randomValues = crypto.getRandomValues(array)
+  let unpadded = randomValues[0].toString(16)
+  let key = '00000000'.slice(unpadded.length) + unpadded
 
-  let parsedBody = await request.json();
+  let parsedBody = await request.json()
   if ('url' in parsedBody) {
-    await SHORTEN.put(key, parsedBody.url, {expirationTtl: 86400});
+    await SHORTEN.put(key, parsedBody.url, { expirationTtl: 86400 })
     let responseBody = {
       message: 'Link shortened successfully',
-      key
+      key,
     }
     return new Response(JSON.stringify(responseBody), {
-      headers: {'content-type': 'application/json'}
-    });
+      headers: { 'content-type': 'application/json' },
+      status: 200,
+    })
   }
-});
+})
 
 async function handleEvent(event) {
-  let requestUrl = new URL(event.request.url);
-  if (requestUrl.pathname === '/' || requestUrl.pathname.includes("static")) {
-    return await getAssetFromKV(event);
+  let requestUrl = new URL(event.request.url)
+  if (requestUrl.pathname === '/' || requestUrl.pathname.includes('static')) {
+    return await getAssetFromKV(event)
   } else {
-    return await router.handle(event.request);
+    return await router.handle(event.request)
   }
 }
 
 addEventListener('fetch', event => {
-  event.respondWith(handleEvent(event));
-});
+  event.respondWith(handleEvent(event))
+})
